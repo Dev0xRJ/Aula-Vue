06ab3284e4974df4daf98c9cebe7f7006d6e572a
R.Jota <eurj.lima@gmail.com>
2025-10-17 02:24:54 -0300

    fix: Corrige vite.config.js com caminho explícito para index.html

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [vue()],
+  root: './',
+  build: {
+    rollupOptions: {
+      input: resolve(__dirname, 'index.html')
+    }
+  },
   resolve: {
     alias: {
-      '@': '/src'
+      '@': resolve(__dirname, 'src')
     }
   }
 })
